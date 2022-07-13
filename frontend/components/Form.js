88937aaba7a15b1192cdbcdf54e1b2802c88f709
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-07-12 22:01:28 -0400

    Works but has 1 bug

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,7 +1,30 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+
+  clickHandler = () => {
+    this.props.toggleDisplayCompleted()
+  }
+
   render() {
-    return null
+    //console.log(this.props.state)
+
+    return (
+      <div>
+        <form onSubmit={this.props.postNewTodo}>
+          {/*console.log(this.props)*/}
+
+          <input type="text" placeholder="Type todo" value={this.props.state.todoNameInput} onChange={this.props.NameInputChange} />
+
+          <button>Add</button>
+        </form>
+
+        { //This works but only if all items are checked off. I spent a couple of hours trying to figure out why it bugged out but I am not sure. :(
+          /*
+          <button onClick={this.clickHandler}>{this.props.state.displayCompleted ? 'Hide' : 'Show'} Completed</button>
+          */
+        }
+      </div>
+    )
   }
 }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
 export default class Todo extends React.Component {
+
+  clickHandler = () => {
+    this.props.toggleCompleted(this.props.todo.id)
+  }
+
   render() {
-    return null
+    //console.log(this.props.todo.id)
+
+    return (
+      <div>
+
+        <li onClick={this.clickHandler}>{this.props.todo.name}
+          {this.props.todo.completed ? <span> ✔️ </span> : <span></span>}
+        </li>
+      </div>
+    )
   }
 }
